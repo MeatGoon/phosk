fe1ce263636a6b5f15718ca91aef059bc0b488d0
taojun666 <ekrhkdektm@naver.com>
2022-04-19 13:22:46 +0900

    04-19
    backup

--- src/main/java/com/teamproject/phosk/junoptiondemo/QRTestController.java
+++ src/main/java/com/teamproject/phosk/junoptiondemo/QRTestController.java
@@ -1,5 +1,43 @@
-/*
- * @RequestMapping("/createCode.do") public ModelAndView
- * createCode(@RequestParam String content){ //ModelAndView 바로 리턴 return new
- * ModelAndView("qrcodeview", "content", content); }
+package com.teamproject.phosk.junoptiondemo;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
  */
+/*@Controller
+public class QRTestController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(QRTestController.class);
+	
+
+	@RequestMapping(value = "/jundemo/QRtest", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "./jundemo/QRtest";
+	}
+	@RequestMapping("/qr")
+	public ModelAndView qrcode(@RequestParam String content) {
+		return new ModelAndView("qrcodeview","content",content);
+	}
+	
+}*/
---
+++ src/main/webapp/WEB-INF/views/jundemo/cateselect.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<alticle>
+	<ul class="menu">
+		<li class="menu_fam"><a class="menu__link" href="tab1/">인기</a></li>
+		<li class="menu_main"><a class="menu__link" href="tab2/">메인</a></li>
+		<li class="menu_side"><a class="menu__link" href="tab3/">사이드</a></li>
+		<li class="menu_drink"><a class="menu__link" href="tab4/">드링크</a></li>
+	</ul>
+</alticle>
+
+
+</body>
+</html>
